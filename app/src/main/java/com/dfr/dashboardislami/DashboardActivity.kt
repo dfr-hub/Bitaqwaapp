94fcc4200da57209c74c02db158d92d45f145914
Fathur Rahman <fathurrhm88@gmail.com>
2021-09-10 14:51:00 +0700

    Initial commit

---
+++ app/src/main/java/com/dfr/dashboardislami/DashboardActivity.kt
@@ -0,0 +1,12 @@
+package com.dfr.dashboardislami
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DashboardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+
+    }
+}
